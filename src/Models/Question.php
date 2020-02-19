6fee07727edf5bf3a0b7c7c30743e8769bd79913
Swapnil Sarwe <swapnilsarwe@gmail.com>
2020-02-20 01:45:34 +0530

    wip

--- src/Models/Question.php
+++ src/Models/Question.php
@@ -9,10 +9,6 @@ class Question extends Model
 {
     use Sushi;
 
-    protected $casts = [
-        'question' => 'string',
-    ];
-
     public function getRows()
     {
         return json_decode(file_get_contents(__DIR__.'/../../vendor/v-graupera/1on1-questions/questions.json'), true);
